8ba35a2ba7094403a7044873f1b469f3ccd615ac
shresth verma <shresth21oct@hotmail.com>
2021-08-29 00:48:32 +0530

    add extra colors

---
+++ README.md
@@ -0,0 +1,3 @@
+# simple-avatar-generator
+An oversimplified avatar generator that takes the name of the user for whom the avatar is being made, as an input, and generates round avatars.
+
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const avatarGenerator = (name) => {
-    const svg = `<svg width="200" xmlns="http://www.w3.org/2000/svg" height="200">
-    <circle cx="50" cy="50" r="40" stroke="green" stroke-width="0" fill="#083DB7"> </circle>
-    <text x="36" y="62" font-size="40px" fill="white" font-family="sans-serif">${name[0]}</text>
+  const colors = ["#139181",
+    "#56003D",
+    "#9EADC8",
+    "#EA8A1F",
+    "#167498",
+    "#000002",
+    "#FF5A2B",
+    "#2B5564",
+    "#210A2D",
+    "#083DB7"
+  ]
+  const svg = `<svg width="200" xmlns="http://www.w3.org/2000/svg" height="200">
+    <circle cx="50" cy="50" r="40" stroke="green" stroke-width="0" fill="${colors[Math.floor(Math.random()*colors.length)]}"> </circle>
+    <text x="50" y="55" font-size="40px" fill="white" font-family="sans-serif" text-anchor="middle" alignment-baseline="middle">${name[0].toUpperCase()}</text>
   </svg>`
-    return "data:image/svg+xml," + encodeURIComponent(svg);
+  return "data:image/svg+xml," + encodeURIComponent(svg);
 }
 
 export default avatarGenerator;
